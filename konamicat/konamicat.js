1aeebf10a93333c88ce71cb54677c553abd3b4c5
Parker Dyer <dyer.lp@gmail.com>
2018-04-09 22:28:12 -0400

    commit

--- konamicat/konamicat.js
+++ konamicat/konamicat.js
@@ -1,27 +1,27 @@
 (function($) {
-$.fn.dinosaur_konami = function(trex) {
+$.fn.cat_konami = function(tabby) {
 if (window.addEventListener) {
 var species = $.extend({
-dino: "http://thecatapi.com/api/images/get?format=src&type=gif",
+kitty: "http://thecatapi.com/api/images/get?format=src&type=gif",
 height: "500",
 width: "500",
 top: "50%",
 left: "50%",
-}, trex),
+}, tabby),
 keys = [],
-konami_dino = "38,38,40,40,37,39,37,39,66,65";
+konami_cat = "38,38,40,40,37,39,37,39,66,65";
 window.addEventListener("keydown", function(e) {
 keys.push(e.keyCode);
-if (keys.toString().indexOf(konami_dino) >= 0) {
-add_dinosaur();
-style_dino();
-$('.dinosaur_konami_trex').fadeIn();
+if (keys.toString().indexOf(konami_cat) >= 0) {
+add_cat();
+style_cat();
+$('.cat_konami_tabby').fadeIn();
 keys = [];
 }
 }, true);
 }
-function style_dino() {
-$('.dinosaur_konami_trex').css({
+function style_cat() {
+$('.cat_konami_tabby').css({
 'position': 'absolute',
 'left': species.left,
 'top': species.top,
@@ -33,40 +33,40 @@ $('.dinosaur_konami_trex').css({
 'z-index': '9999',
 });
 }
-function add_dinosaur() {
-var t_rex = $('<div class="dinosaur_konami_trex"><img src="' + species.dino + '" height="100%" /></div>');
-$(t_rex).appendTo('body');
-move_dinosaur();
-$('.dinosaur_konami_trex').click(function() {
+function add_cat() {
+var kitten = $('<div class="cat_konami_tabby"><img src="' + species.kitty + '" height="100%" /></div>');
+$(kitten).appendTo('body');
+move_cat();
+$('.cat_konami_tabby').click(function() {
 $(this).fadeOut().remove();
 });
 }
-function move_dinosaur() {
+function move_cat() {
 window.addEventListener("keydown", function(e) {
 switch (e.which) {
 case 37: //left
-$('.dinosaur_konami_trex').stop().animate({
+$('.cat_konami_tabby').stop().animate({
 left: '-=100',
 duration: 'slow',
 easing: 'swing',
 });
 break;
 case 38: //up
-$('.dinosaur_konami_trex').stop().animate({
+$('.cat_konami_tabby').stop().animate({
 top: '-=100',
 duration: 'slow',
 easing: 'swing',
 });
 break;
 case 39: //right
-$('.dinosaur_konami_trex').stop().animate({
+$('.cat_konami_tabby').stop().animate({
 left: '+=100',
 duration: 'slow',
 easing: 'swing',
 });
 break;
 case 40: //bottom
-$('.dinosaur_konami_trex').stop().animate({
+$('.cat_konami_tabby').stop().animate({
 top: '+=100',
 duration: 'slow',
 easing: 'swing',
